1ae70098830ac28e2da65af530e192323d9c021b
임석현 <sjsh1623@flow.team>
2023-09-24 23:04:08 +0900

    feat (ios_googlemap) : Get User Current Location and Pin them Study Note: [Problem] 1. MapView State로 관리하려 했으나 바로 동작하지 않음
    [Solution]
    1. 사용자 Location에 설정될때까지 기다렸다가 Rendering 하게 함

--- App.tsx
+++ App.tsx
@@ -1,32 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import {NavigationContainer} from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import {StyleSheet, Text, View} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps";
+import {useEffect, useState, useRef} from "react";
+import Geolocation from "react-native-geolocation-service";
+
+interface ILocation {
+    latitude: number;
+    longitude: number;
+}
 
 export default function App() {
-  return (
-      <>
-          <View style={{ flex: 1 }}>
-              <MapView
-                  style={{ flex: 1 }}
-                  provider={PROVIDER_GOOGLE}
-                  initialRegion={{
-                      latitude: 37.78825,
-                      longitude: -122.4324,
-                      latitudeDelta: 0.0922,
-                      longitudeDelta: 0.0421,
-                  }}
-              />
-          </View>
-      </>
-  );
+    const [location, setLocation] = useState<ILocation | undefined>();
+    const [markers, setMarkers] = useState([])
+        useEffect(() => {
+            Geolocation.requestAuthorization("whenInUse").then(() => {
+                Geolocation.getCurrentPosition(
+                    (position) => {
+                        const {latitude, longitude} = position.coords;
+                        setLocation({
+                            latitude,
+                            longitude,
+                        });
+                    },
+                    error => {
+                        console.log(error.code, error.message);
+                    },
+                    {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+                );
+            })
+        }, []);
+
+
+    if (!location) {
+        return (
+            <View>
+                <Text>Splash Screen</Text>
+            </View>
+        );
+    }
+
+    return (
+        <>
+            <View style={{flex: 1}}>
+                <MapView
+                    style={{flex: 1}}
+                    provider={PROVIDER_GOOGLE}
+                    initialRegion={{
+                        latitude: location.latitude,
+                        longitude: location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                    onPress={(location) => {
+                        const { coordinate } = location.nativeEvent;
+                        const newMarker = {
+                            coordinate,
+                            title: '새로운 마커', // 마커의 제목
+                            description: '이것은 새로운 마커입니다.', // 마커의 설명
+                        };
+                        setMarkers([...markers, newMarker]); // 새로운 마커를 마커 목록에 추가
+                        console.log(markers)
+                    }}
+                >
+                    {markers.map((marker, index) => (
+                        <Marker
+                            key={index}
+                            coordinate={marker.coordinate}
+                            title={marker.title}
+                            description={marker.description}
+                        />
+                    ))}
+                </MapView>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
