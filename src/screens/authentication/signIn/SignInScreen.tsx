80e712d0b3a90e2b012bca8a362ab3f7c8b9a15e
임석현 <sjsh1623@flow.team>
2023-11-15 00:02:00 +0900

    Input Customizing

--- src/screens/authentication/signIn/SignInScreen.tsx
+++ src/screens/authentication/signIn/SignInScreen.tsx
@@ -1,49 +1,44 @@
 import AuthenticationTemplate from "../AuthenticationTemplate";
 import {TextInput} from 'react-native-paper';
 import {Text, StyleSheet, View, SafeAreaView, TouchableOpacity, Image} from "react-native";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
 export default function SignInScreen() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
 
     const context: React.FC = () => (
         <View style={{width: '100%', backgroundColor: '#28282B'}}>
-            <TextInput
-                label={'이메일 주소'}
-                textColor={'white'}
-                contentStyle={{backgroundColor: '#28282B', height: 60, paddingTop: 30}}
-                cursorColor={'white'}
-                underlineColor={'white'}
-                placeholderTextColor={'#A8AAAA'}
-                placeholder={'abc@space-parking.com'}
-                activeUnderlineColor={'white'}
-                underlineStyle={{borderColor: 'white'}}
-                autoFocus={true}
-                theme={{
-                    colors: {
-                        background: '#28282B',
-                        onSurfaceVariant: 'white',
-                    },
-                }}
-
-            />
-            <TextInput
-                label={'비밀번호'}
-                textColor={'white'}
-                contentStyle={{backgroundColor: '#28282B', height: 60, paddingTop: 30}}
-                cursorColor={'white'}
-                underlineColor={'white'}
-                placeholderTextColor={'#A8AAAA'}
-                activeUnderlineColor={'white'}
-                underlineStyle={{borderColor: 'white'}}
-                autoFocus={true}
-                theme={{
-                    colors: {
-                        background: '#28282B',
-                        onSurfaceVariant: 'white',
-                    },
-                }}
-                placeholder={'영문, 숫자, 특수문자'}
-            />
+            <View>
+                <Text style={{color: 'white', marginLeft:15, zIndex: 10, marginBottom:-7}}>이메일</Text>
+                <TextInput
+                    ref={emailRef}
+                    textColor={'white'}
+                    mode={"flat"}
+                    style={{marginTop:-3}}
+                    contentStyle={{backgroundColor: '#28282B'}}
+                    cursorColor={'white'}
+                    underlineColor={'white'}
+                    placeholderTextColor={'#A8AAAA'}
+                    activeUnderlineColor={'white'}
+                    underlineStyle={{borderColor: 'white'}}
+                    placeholder={'abc@space-parking.com'}
+                />
+            </View>
+            <View style={{marginTop:50}}>
+                <Text style={{color: 'white', marginLeft:15, zIndex: 10, marginBottom:-7}}>비밀번호</Text>
+                <TextInput
+                    ref={passwordRef}
+                    textColor={'white'}
+                    contentStyle={{backgroundColor: '#28282B'}}
+                    cursorColor={'white'}
+                    underlineColor={'white'}
+                    placeholderTextColor={'#A8AAAA'}
+                    activeUnderlineColor={'white'}
+                    underlineStyle={{borderColor: 'white'}}
+                    placeholder={'영문, 숫자, 특수문자'}
+                />
+            </View>
         </View>
     )
     return (
