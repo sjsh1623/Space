2f27d4d4eefff8264299986ce0aade40c059302c
Andrew <sjsh1623@gmail.com>
2024-01-15 13:46:54 +0900

    feat (View) : + Create a view for detail and outline

---
+++ src/screens/main/detail/index.tsx
@@ -0,0 +1,29 @@
+import {exp} from "@gorhom/bottom-sheet/lib/typescript/utilities/easingExp";
+import {View, Text} from "react-native";
+
+const index = () => {
+
+    const stars = (rate: number) => {
+
+    }
+
+    const favorite = (count: number) => {
+
+    }
+
+    return (
+        <View>
+            <View>
+                <Text>무언가</Text>
+                <View></View>
+                <View>28분 거리</View>
+            </View>
+
+            <View>
+
+            </View>
+        </View>
+    )
+}
+
+export default index;
